d346fd360225546262f2a5e98aeacd959ebd5b59
Atuma David <atumadavid35@gmail.com>
2024-09-23 10:04:24 +0100

    sidebar for trending topics and who to follow

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,6 +4,7 @@ import PostEditor from "@/components/posts/editor/PostEditor";
 import prisma from "@/lib/prisma";
 import Posts from "@/components/posts/Posts";
 import { PostsDataInclude } from "@/lib/types";
+import TrendsSidebar from "@/components/TrendsSidebar";
 
 export default async function Home() {
   const posts = await prisma.post.findMany({
@@ -21,13 +22,14 @@ export default async function Home() {
   });
 
   return (
-    <main className="w-full min-w-0">
+    <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         {posts.map((post) => (
           <Posts key={post.id} post={post} />
         ))}
       </div>
+      <TrendsSidebar />
     </main>
   );
 }
